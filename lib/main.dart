5ac9d993c59ae9e07b37baf9564f906204089fbe
kshi9 <kshi9478@gmail.com>
2025-11-09 20:13:39 +0900

    🔒add Naver, test. (local ok)

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:cultureyo/src/features/authentication/domain/usecases/naver_logi
 import 'package:cultureyo/src/features/authentication/presentation/pages/splash_page.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_naver_login/flutter_naver_login.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';

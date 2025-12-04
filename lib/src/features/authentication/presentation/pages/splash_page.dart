f58bef72d1407a745da36a63e1805b8688c741a6
sangyeop1001 <162437147+sangyeop1001@users.noreply.github.com>
2025-12-04 18:56:54 +0900

    Add files via upload

---
+++ lib/src/features/authentication/presentation/pages/splash_page.dart
@@ -0,0 +1,45 @@
+import 'package:cultureyo/src/features/authentication/domain/usecases/auth_manager.dart';
+import 'package:cultureyo/src/features/home.dart';
+import 'package:flutter/material.dart';
+import 'login_page.dart';
+import 'package:provider/provider.dart';
+
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(() async {
+      final authManager = context.read<AuthManager>();
+      await authManager.checkAuth();
+
+      if (!mounted) return;
+      switch (authManager.status) {
+        case AuthStatus.kakao:
+        case AuthStatus.naver:
+          Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (_) => MainScreen()));
+          break;
+        case AuthStatus.none:
+          Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (_) =>  const LoginPage()));
+          break;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Center(child: CircularProgressIndicator())
+    );
+  }
+}

f3c39d900c473a1a309b363d7d8e2c897be70c21
kshi9 <kshi9478@gmail.com>
2025-12-10 11:58:10 +0900

    hotfix kts & birthYear

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -5,25 +8,48 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+val envFile = rootProject.file(".env")
+val props = Properties()
+if (envFile.exists()) {
+//    FileInputStream(envFile).use { props.load(it) }
+    props.load(FileInputStream(envFile))
+}
+val kakaoKey: String = props.getProperty("KAKAO_NATIVE_APP_KEY") ?: ""
+val naverClientId: String = props.getProperty("NAVER_CLIENT_ID") ?: ""
+val naverClientSecret: String = props.getProperty("NAVER_CLIENT_SECRET") ?: ""
+val naverClientName: String = props.getProperty("NAVER_CLIENT_NAME") ?: ""
+
+
 android {
-    namespace = "com.example.cultureyo"
+    namespace = "S25osp.kr.ac.kumoh.dartroll_front"
+
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.cultureyo"
+        applicationId = "S25osp.kr.ac.kumoh.dartroll_front"
         // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        // For more information,  see: https://flutter.dev/to/review-gradle-config.
+
+        resValue("string", "kakao_native_app_key", kakaoKey)
+        manifestPlaceholders["KAKAO_NATIVE_APP_KEY"] = kakaoKey
+
+        resValue("string", "client_id", naverClientId)
+        resValue("string", "client_name", naverClientName)
+        resValue("string", "client_secret", naverClientSecret)
+        manifestPlaceholders["CLIENT_SECRET"] = naverClientSecret
+        manifestPlaceholders["CLIENT_ID"] = naverClientId
+        manifestPlaceholders["CLIENT_NAME"] = naverClientName
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
